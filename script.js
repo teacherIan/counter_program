9770104eed986ca13df6647cee8623dd8c081546
teacherIan <ian_malloy@ssis.asia>
2022-06-16 12:21:40 +0800

    Minor Updates

--- script.js
+++ script.js
@@ -3,25 +3,30 @@ const scoreContainer = document.getElementById('score-container');
 const addContainer = document.getElementById('add-container');
 const subtractContainer = document.getElementById('subtract-container');
 
-// console.log(score);
-// console.log(scoreContainer);
-// console.log(addContainer);
-// console.log(subtractContainer);
-
 let counter = 0;
+
+fetch('http://127.0.0.1:9999/get').then((response) => {
+  response.json().then((data) => {
+    counter = data.counter;
+    updateScore();
+  });
+});
 
 scoreContainer.addEventListener('click', () => {
   counter = 0;
+  fetch('http://127.0.0.1:9999/reset', { method: 'POST' });
   updateScore();
 });
 
 addContainer.addEventListener('click', () => {
   counter += 1;
+  fetch('http://127.0.0.1:9999/add', { method: 'POST' });
   updateScore();
 });
 
 subtractContainer.addEventListener('click', () => {
   counter -= 1;
+  fetch('http://127.0.0.1:9999/subtract', { method: 'POST' });
   updateScore();
 });
 
